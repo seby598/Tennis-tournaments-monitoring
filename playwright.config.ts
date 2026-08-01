e64efaa5d5b9891ca00060f59333aadd3c56c7e7
Sebastian Stancu <seby598@gmail.com>
2026-08-01 02:09:37 +0200

    Updated config to generate list without OS info

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,8 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // This forces Playwright to drop the OS and browser suffixes
+  snapshotPathTemplate: '{testDir}/{testFileName}-snapshots/{arg}{ext}',
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -39,15 +41,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
